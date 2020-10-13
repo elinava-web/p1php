69bc2ac041c7cdcc49f44aa8c718e4850cb330ab
elinava-web <69087743+elinava-web@users.noreply.github.com>
2020-10-13 04:07:39 -0600

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>01 Nava Eliseo</title>
+    
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="img/logo4.png" alt="FalconMasters"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index2.php">Inicio</a></li>
+					<li><a href="p1.html">P1</a></li>
+					<li><a href="p2.html">P2</a></li>
+					<li><a href="p3.html">P3</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Bienvenidos! al Cbtis No 128</h1>
+			</div>
+
+			<div class="articulo">
+				<article>
+					<p>Descripcion de problema 1 BLA BLa</p>
+					<br/>
+					<p>Descripcion de problema 2 BLA BLa
+					</p>
+					<br/>
+					<p>Descripcion de problema 3 BLA BLa</p>
+					<br/>
+					<p>Descripcion de problema 4 BLA BLa</p>
+				</article>
+			</div>
+
+			<aside>
+				<div class="widget">
+					<h3>Manual de practicas PHP</h3>
+					<ul>
+						<li><a href="#">P1 Area circulo</a></li>
+						<li><a href="#">P2 Area Triangulo</a></li>
+						<li><a href="#">P3 Area Cuadrado</a></li>
+						<li><a href="#">P4 Ejemplo if else</a></li>
+						<li><a href="#">p5 Ejemplo while</a></li>
+					</ul>
+				</div>
+			</aside>
+		</div>
+	</section>
+	<footer>
+		<div class="wrapp">
+            <p> No lista 01 </p>
+            <p> // SUBMÓDULO 2
+                Desarrolla aplicaciones Web con conexión a bases de datos</p>
+            <p>https://github.com/elinava-web </p>
+		</div>
+	</footer>
+</body>
+</html>
